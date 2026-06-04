d1d93dc07ae00ae0e6328fb5b379602f3e9cd3d9
AHRaunok <rsraunok2@gmail.com>
2026-06-05 00:59:59 +0600

    mongodb

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+'use client'
 
+import Link from 'next/link';
+import React from 'react';
+import { useForm } from "react-hook-form"
 const Login = () => {
+
+const loginfunction = (data) => {
+    // Login logic here
+    console.log('Email:', data.email);
+    console.log('Password:', data.password);
+    console.log(data);
+}
+ const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='container mx-auto my-10  bg-gray-400 p-10 rounded-lg'>
+            <form className='flex flex-col items-center justify-center' onSubmit={handleSubmit(loginfunction)}>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <h1>LOG-IN</h1>
+
+                    <label className="label">Email</label>
+                    <input type="email" className="input"  {...register("email", { required: "Email is required" })} placeholder="Email" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+
+                    
+                    <label className="label">Password</label>
+                    <input type="password" className="input" {...register("password", { required: "Password is required" })} placeholder="Password" />
+                    {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+
+
+
+                    <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+            </form>
+            <p className="mt-2 text-center ">Dont have an account ? <Link href="/register" className="link link-hover">Register</Link>   </p>
         </div>
     );
 };
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+'use client'
 
+import Link from 'next/link';
+import React from 'react';
+import { useForm } from "react-hook-form"
 const Register = () => {
+
+const registerfunction = (data) => {
+    // Register logic here
+    console.log('Email:', data.email);
+    console.log('Password:', data.password);
+    console.log(data);
+}
+ const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='container mx-auto my-10  bg-gray-400 p-10 rounded-lg'>
+            <form className='flex flex-col items-center justify-center' onSubmit={handleSubmit(registerfunction)}>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <h1>REGISTER</h1>
+
+                    <label className="label">Name</label>
+                    <input type="text" className="input"  {...register("name", { required: "Name is required" })} placeholder="Name" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+
+
+                    <label className="label">Photo URL</label>
+                    <input type="text" className="input"  {...register("photourl", { required: "Photo URL is required" })} placeholder="Photo URL" />
+                    {errors.photourl && <p className='text-red-500'>{errors.photourl.message}</p>}
+
+
+                    <label className="label">Email</label>
+                    <input type="email" className="input"  {...register("email", { required: "Email is required" })} placeholder="Email" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+
+                    
+                    <label className="label">Password</label>
+                    <input type="password" className="input" {...register("password", { required: "Password is required" })} placeholder="Password" />
+                    {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+
+
+
+                    <button className="btn btn-neutral mt-4">Register</button>
+            </fieldset>
+            </form>
+            <p className="mt-2 text-center ">Already have an account ? <Link href="/login" className="link link-hover">Login</Link>   </p>
         </div>
     );
 };
